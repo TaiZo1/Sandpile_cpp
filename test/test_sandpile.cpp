5953bd6ba8a6385c404aaba2f3b7f047c65430a8
Lucas Lacharme <80881483+TaiZo1@users.noreply.github.com>
2025-07-09 16:01:01 +0200

    Update test_sandpile.cpp

--- test/test_sandpile.cpp
+++ test/test_sandpile.cpp
@@ -8,12 +8,12 @@
 int main(){
     mt19937 G(time(nullptr));
     Sandpile s(8,8,5);
-    std::cout << "La hauteur de sable en (1,1) est " << s(2,1) << std::endl;
-    std::cout << "La configuration complète est :" << std::endl << s;
+    std::cout << "The sand height at (1,1) is " << s(2,1) << std::endl;
+    std::cout << "The full configuration is:" << std::endl << s;
 
-    cout << "Le nombre de voisins de (0,0) est " << s.voisins({0,0}).size() << endl;
+    cout << "The number of neighbors of (0,0) is " << s.voisins({0,0}).size() << endl;
 
-    cout << "Le nombre de voisins de (1,2) est " << s.voisins({1,2}).size() << endl;
+    cout << "The number of neighbors of (1,2) is " << s.voisins({1,2}).size() << endl;
 
     ofstream Fichier("stab_8x8_5.txt");
 
