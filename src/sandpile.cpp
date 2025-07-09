3493780c070e1179b9fbc188645752c2b848fcf6
Lucas Lacharme <80881483+TaiZo1@users.noreply.github.com>
2025-07-09 15:59:10 +0200

    Add sandpile.cpp with Sandpile method implementations
    Implements the constructor, stabilization logic, neighbor calculation, eboulement mechanism, and random grain addition.

---
+++ src/sandpile.cpp
@@ -0,0 +1,36 @@
+#include "sandpile.hpp"
+
+#include <fstream>
+#include <ctime>
+
+
+
+int main(){
+    mt19937 G(time(nullptr));
+    Sandpile s(8,8,5);
+    std::cout << "The sand height at (1,1) is " << s(2,1) << std::endl;
+    std::cout << "The full configuration is:" << std::endl << s;
+
+    cout << "The number of neighbors of (0,0) is " << s.voisins({0,0}).size() << endl;
+
+    cout << "The number of neighbors of (1,2) is " << s.voisins({1,2}).size() << endl;
+
+    ofstream Fichier("stab_8x8_5.txt");
+
+    s.stabil();
+
+    Fichier << s.nb_total_ebouls() << endl;
+    Fichier << s;
+
+    Sandpile t(100,100,4);
+
+    t.stabil();
+
+    cout << t << endl;
+
+    t.add_random_grain(G,3);
+
+
+
+    return 0;
+}
