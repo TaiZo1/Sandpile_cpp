d80eb47427dfce53503715e9c1256d67a7cd9ba5
Lucas Lacharme <80881483+TaiZo1@users.noreply.github.com>
2025-07-09 16:03:19 +0200

    Update sandpile.cpp

--- src/sandpile.cpp
+++ src/sandpile.cpp
@@ -1,36 +1,24 @@
 #include "sandpile.hpp"
 
-#include <fstream>
-#include <ctime>
+Sandpile::Sandpile(int m0, int n0, int k): m(m0), n(n0), terrain(m0*n0,k),
+    nb_ebouls(m0*n0,0), next_ebouls(){
+        if(k>=4){
+            for(int i=0; i<m; i++){
+                for(int j =0; j<n; j++){
+                    next_ebouls.push_back(upair(i,j));
+                }
+            }
+        }
+    };
 
 
+ostream& operator<<(ostream& o, const Sandpile& S){
+    for(int j =0; j<S.n; j++){
+        for(int i =0; i<S.m; i++){
+            o << S(i,j);
+        }
+        o << endl;
+    }
 
-int main(){
-    mt19937 G(time(nullptr));
-    Sandpile s(8,8,5);
-    std::cout << "The sand height at (1,1) is " << s(2,1) << std::endl;
-    std::cout << "The full configuration is:" << std::endl << s;
-
-    cout << "The number of neighbors of (0,0) is " << s.voisins({0,0}).size() << endl;
-
-    cout << "The number of neighbors of (1,2) is " << s.voisins({1,2}).size() << endl;
-
-    ofstream Fichier("stab_8x8_5.txt");
-
-    s.stabil();
-
-    Fichier << s.nb_total_ebouls() << endl;
-    Fichier << s;
-
-    Sandpile t(100,100,4);
-
-    t.stabil();
-
-    cout << t << endl;
-
-    t.add_random_grain(G,3);
-
-
-
-    return 0;
-}
+    return o;
+};
